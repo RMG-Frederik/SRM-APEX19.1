9959cc4110129634bf2459dddaf1d1bf8955df65
Frederik Vantroys <46712424+RMG-Frederik@users.noreply.github.com>
2019-06-18 09:28:51 +0200

    initial js commit

--- scripts/pages/SRM_P80.js
+++ scripts/pages/SRM_P80.js
@@ -16,39 +16,56 @@ Rmg.Srm.Page80 = Rmg.Srm.Page80 || {}
  * @example Rmg.Srm.Page80.configTakenLijstGrid(config);
  **/
 Rmg.Srm.Page80.configTakenLijstGrid = function(config) {
-        if (!config.toolbar) {
-            config.toolbar = {};
-        }
-        if (!config.views) {
-            config.views = {};
-        }
-        config.toolbar.actionMenu = false;
-        config.toolbar.searchField = true;
-        return config;
+    if (!config.toolbar) {
+        config.toolbar = {};
     }
-    /**
-     * @function buildStatusFilter
-     * @example Rmg.Srm.Page80.buildStatusFilter();
-     **/
-Rmg.Srm.Page80.buildStatusFilter = function() {
-    var vRegion = apex.region("view_tasks");
-    var vFilters = vRegion.call("getFilters");
-    var vGrid = vRegion.call("getViews", "grid");
-    var vFilterColId = vGrid.modelColumns["TAAK_STATE_CODE"].id;
-    for (var i in vFilters) {
-        if (vFilters[i].columnId == vFilterColId) {
-            vRegion.call("deleteFilter", vFilters[i].id);
-        }
+    if (!config.views) {
+        config.views = {};
     }
+    config.toolbar.actionMenu = true;
+    config.toolbar.searchField = true;
+    config.toolbarData = [{
+            controls: [{
+                type: "BUTTON",
+                action: "show-filter-dialog",
+                iconBeforeLabel: true
+            }]
+        },
+        {
+            groupTogether: true,
+            controls: [{
+                    type: "TEXT",
+                    id: "search_field",
+                    enterAction: "search"
+                },
+                {
+                    type: "BUTTON",
+                    action: "search"
+                }
+            ]
+        }
+    ];
+    var $ = apex.jQuery,
+        toolbarData = $.apex.interactiveGrid.copyDefaultToolbar(),
+        toolbarGroup = toolbarData[toolbarData.length - 1]; // this is the last group with reset button
 
-    if ($v2('P11_DDL_STATUS') != 'ALL') {
-        vRegion.call("addFilter", {
-            type: 'column',
-            columnType: 'column',
-            columnName: 'TAAK_STATE_CODE',
-            operator: 'EQ',
-            value: $v2('P11_DDL_STATUS'),
-            isCaseSensitive: false
-        })
+    // add our own button
+    toolbarGroup.controls.push({
+        type: "BUTTON",
+        action: "my-action"
+    });
+    config.toolbarData = toolbarData;
+
+    config.initActions = function(actions) {
+        // can modify state of existing actions or add your own
+        // can also pass in an array of actions to add
+        actions.add({
+            name: "my-action",
+            label: "Hello",
+            action: function(event, focusElement) {
+                alert("Hello World!");
+            }
+        });
     }
+    return config;
 }
